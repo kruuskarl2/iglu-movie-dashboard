2535f228129b7868f07e922f635314354edb5894
kruuskarl2 <karlkruus0@gmail.com>
2023-05-30 11:03:12 +0300

    Add font, restructuring project

---
+++ src/components/App/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
