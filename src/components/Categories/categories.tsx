5da38f3719b32cd17b1cc3417095251036e53e66
kruuskarl2 <karlkruus0@gmail.com>
2023-05-31 11:43:22 +0300

    Add categories request

--- src/components/Categories/categories.tsx
+++ src/components/Categories/categories.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './categories.scss';
-import data from './dummyData';
+
+interface Genre {
+    name: string;
+    id: number;
+}
 
 function Categories() {
-    const { genres } = data;
-    genres.splice(5);
+    const [genres, setGenres] = useState([]);
+    const [moreGenres, setMoreGenres] = useState([]);
 
-    const dropdownCategoriesJSX = genres.map((genre) => {
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization:
+                    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNmRlZTA2MDQyYjM0MjZjMWIxYTUyZWUzNDJiYjBjZiIsInN1YiI6IjY0NzQ5ZjczY2MyNzdjMDExNjFjZWIxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1PJTncFPCiXKvecugtzed7wIPETWBiNlGwIv4bYEoSE',
+            },
+        };
+
+        fetch(
+            'https://api.themoviedb.org/3/genre/movie/list?language=en',
+            options
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                const genres = response?.genres;
+                setMoreGenres(genres.splice(5));
+                setGenres(genres);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, []);
+
+    if (!genres.length || !moreGenres.length) {
+        return <div className="categories hidden" />;
+    }
+
+    const dropdownCategoriesJSX = moreGenres.map((genre: Genre) => {
         const { id, name } = genre;
 
         return (
@@ -17,7 +50,7 @@ function Categories() {
         );
     });
 
-    const categoriesJSX = genres.map((genre) => {
+    const categoriesJSX = genres.map((genre: Genre) => {
         const { id, name } = genre;
 
         return (
@@ -30,10 +63,10 @@ function Categories() {
     return (
         <div className="categories">
             <div className={'category-container'}>
-                <div className={'category__active'}>All movies</div>
+                <div className={'category active'}>All movies</div>
                 {categoriesJSX}
             </div>
-            <div className={'category__more'}>
+            <div className={'category more'}>
                 More
                 <div className={'dropdown'}>{dropdownCategoriesJSX}</div>
             </div>
